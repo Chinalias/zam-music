75252d1e81f994e834738dffe7423a4680a9ebbc
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-08-23 15:35:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '$';
 client.on('ready', function() {
-    client.user.setGame(` $help - Promise `,"http://twitch.tv/Baktash_183")
+    client.user.setGame(` $help - 4 Fun `,"http://twitch.tv/Baktash_183")
     console.log(`i am ready ${client.user.username}`);
 });
 /*
