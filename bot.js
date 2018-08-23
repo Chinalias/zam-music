c3feeb0b7ac31098a56a674cd6601557dae4a344
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-08-23 17:34:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -258,7 +258,7 @@ client.on('message', message => {
  
  
 const adminprefix = "$$vip";
-const devs = ['389090790984515594','459203545468764160'];
+const devs = ['331081268731052042','459203545468764160'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
